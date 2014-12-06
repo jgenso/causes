db6b5438b9b1776b170854a94d4978e457501735
andrea_genso <andrea@genso.com.bo>
2014-12-06 12:12:00 -0400

    add photo and tags

--- src/main/scala/code/model/Cause.scala
+++ src/main/scala/code/model/Cause.scala
@@ -1,9 +1,17 @@
 package code
 package model
 
-import net.liftweb.mongodb.record.field.{DateField, ObjectIdPk}
+import com.mongodb.gridfs.GridFS
+import net.liftweb.common.Full
+import net.liftweb.http.{FileParamHolder, SHtml}
+import net.liftweb.mongodb.MongoDB
+import net.liftweb.mongodb.record.field.{MongoListField, DateField, ObjectIdPk}
 import net.liftweb.mongodb.record.{MongoRecord, MongoMetaRecord}
 import net.liftweb.record.field._
+import net.liftweb.util.DefaultConnectionIdentifier
+import org.bson.types.ObjectId
+
+import scala.xml.NodeSeq
 
 /**
  * Created by andrea on 12/6/14.
@@ -25,6 +33,14 @@ class Cause private() extends MongoRecord[Cause] with ObjectIdPk[Cause] {
 
     override def validations =
       valMaxLen(1000, "Description must be 1000 characters or less") _ ::
+        super.validations
+  }
+
+  object slogan extends StringField(this, 100) {
+    override def displayName = "Slogan"
+
+    override def validations =
+      valMaxLen(100, "Name must be 100 characters or less") _ ::
         super.validations
   }
 
@@ -67,7 +83,40 @@ class Cause private() extends MongoRecord[Cause] with ObjectIdPk[Cause] {
 
   }
 
+  object tags extends MongoListField[Cause, ObjectId](this)
+
   object rate extends IntField(this)
+
+  object photo extends StringField(this, 500) {
+    override def displayName = "Photo"
+    private def photoHtml =
+      <div class="image">
+        <img class="img-responsive" src={s"/images/user/profile/${id.get}"} alt={s"${owner.name.get}'s photo"}/>
+      </div>
+    private def elem = {
+      (value.trim match {
+        case "" =>
+          NodeSeq.Empty
+        case other =>
+          photoHtml
+      }) ++
+        SHtml.fileUpload(
+          fph => {
+            set(savePhoto(fph))
+          }
+        )
+    }
+    override def toForm = Full(elem)
+    override def asHtml = photoHtml
+    private def savePhoto(fph: FileParamHolder): String = {
+      MongoDB.use(DefaultConnectionIdentifier) { db =>
+        val fs = new GridFS(db)
+        val mongoFile = fs.createFile(fph.fileStream, fph.fileName + " - " + org.apache.commons.codec.digest.DigestUtils.md5Hex(fph.fileStream))
+        mongoFile.save()
+        mongoFile.getFilename
+      }
+    }
+  }
 }
 
 object Cause extends Cause with MongoMetaRecord[Cause] {
--- src/main/scala/code/model/New.scala
+++ src/main/scala/code/model/New.scala
@@ -1,8 +1,16 @@
 package code.model
 
+import code.model.Result.photo._
+import com.mongodb.gridfs.GridFS
+import net.liftweb.common.Full
+import net.liftweb.http.{FileParamHolder, SHtml}
+import net.liftweb.mongodb.MongoDB
 import net.liftweb.mongodb.record.{MongoMetaRecord, MongoRecord}
 import net.liftweb.mongodb.record.field.{ObjectIdRefField, DateField, ObjectIdPk}
-import net.liftweb.record.field.{TextareaField, StringField}
+import net.liftweb.record.field.{OptionalStringField, TextareaField, StringField}
+import net.liftweb.util.DefaultConnectionIdentifier
+
+import scala.xml.NodeSeq
 
 /**
  * Created by andrea on 12/6/14.
@@ -20,6 +28,37 @@ class New private() extends MongoRecord[New] with ObjectIdPk[New] {
 
   object registerDate extends DateField(this){
     override def displayName = "Register date"
+  }
+
+  object photo extends OptionalStringField(this, 500) {
+    override def displayName = "Photo"
+    private def photoHtml =
+      <div class="image">
+        <img class="img-responsive" src={s"/images/user/profile/${id.get}"} alt={s"${id.get}'s new photo"}/>
+      </div>
+    private def elem = {
+      (value.headOption.map(v => v).getOrElse("").trim match {
+        case "" =>
+          NodeSeq.Empty
+        case other =>
+          photoHtml
+      }) ++
+        SHtml.fileUpload(
+          fph => {
+            set(Some(savePhoto(fph)))
+          }
+        )
+    }
+    override def toForm = Full(elem)
+    override def asHtml = photoHtml
+    private def savePhoto(fph: FileParamHolder): String = {
+      MongoDB.use(DefaultConnectionIdentifier) { db =>
+        val fs = new GridFS(db)
+        val mongoFile = fs.createFile(fph.fileStream, fph.fileName + " - " + org.apache.commons.codec.digest.DigestUtils.md5Hex(fph.fileStream))
+        mongoFile.save()
+        mongoFile.getFilename
+      }
+    }
   }
 
   object cause extends ObjectIdRefField(this, Cause)
