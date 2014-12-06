743b00f7028da4047b471e87182daf9d54134a5a
Juan Jose Olivera <juan@genso.com.bo>
2014-12-06 18:26:05 -0400

    added angular  pagination

--- src/main/webapp/scripts/cause/contributors.js
+++ src/main/webapp/scripts/cause/contributors.js
@@ -1,16 +1,24 @@
 (function(angular){
   'use strict';
 
-  var appContributors = angular.module('Contributors', ['ContributorsServer']);
+  var appContributors = angular.module('Contributors', ['ContributorsServer', 'ui.bootstrap']);
 
   appContributors.controller("ContributorsController", function($scope, ServerFuncs, $filter, $log) {
     $log.log("CALLED");
     $scope.page = 0;
     $scope.count = 0;
+    $scope.itemsPerPage = 10;
     $scope.items = [];
+
     $scope.fetchPage = function() {
       ServerFuncs.fetchPage(page);
     };
+
+    $scope.pageChanged = function() {
+      $log.log('Page changed to: ' + $scope.currentPage);
+      $scope.fetchPage();
+    };
+
     $scope.$on('after-fetch-page', function (event, data) {
       $scope.$apply(function () {
         $scope.items = data.items;
--- src/main/webapp/scripts/cause/followers.js
+++ src/main/webapp/scripts/cause/followers.js
@@ -1,16 +1,24 @@
 (function(angular){
   'use strict';
 
-  var appContributors = angular.module('Followers', ['FollowersServer']);
+  var appContributors = angular.module('Followers', ['FollowersServer', 'ui.bootstrap']);
 
   appContributors.controller("FollowersController", function($scope, ServerFuncs, $filter, $log) {
     $log.log("CALLED");
     $scope.page = 0;
     $scope.count = 0;
+    $scope.itemsPerPage = 10;
     $scope.items = [];
+
     $scope.fetchPage = function() {
       ServerFuncs.fetchPage(page);
     };
+
+    $scope.pageChanged = function() {
+      $log.log('Page changed to: ' + $scope.currentPage);
+      $scope.fetchPage();
+    };
+
     $scope.$on('after-fetch-page', function (event, data) {
       $scope.$apply(function () {
         $scope.items = data.items;
