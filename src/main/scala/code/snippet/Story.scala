45e32e7ca6668004c25a15ea06e98e8b035c5abd
andrea_genso <andrea@genso.com.bo>
2014-12-07 11:48:24 -0400

    Story

--- src/main/scala/code/snippet/Story.scala
+++ src/main/scala/code/snippet/Story.scala
@@ -1,20 +1,33 @@
 package code.snippet
 
+import code.menu.CauseMenus
 import code.model.Cause
 import net.liftweb.util
-import scala.xml.NodeSeq
 import util.Helpers._
 /**
  * Created by andrea on 12/6/14.
  */
 
 object Story {
-  val cause: Cause = Cause.createRecord
 
-  def render(html: NodeSeq) =  {
-    "@name" #> cause.name.get &
-    "@description" #> cause.description.get &
-    "@photo" #> cause.photo.photoHtml
+  def image() = {
+    val cause = CauseMenus.causeStory.currentValue openOr Cause.createRecord
+    "data-name=photo *" #> cause.photo.photoHtml
+  }
+  def story() =  {
+    val cause = CauseMenus.causeStory.currentValue openOr Cause.createRecord
+    "data-name=description *" #> cause.description.get
+  }
+
+  def slogan() = {
+    val cause = CauseMenus.causeStory.currentValue openOr Cause.createRecord
+    "data-name=slogan *" #> cause.slogan.get
+  }
+
+  def name() = {
+    val cause = CauseMenus.causeStory.currentValue openOr Cause.createRecord
+
+    "data-name=name *" #> cause.name.get
   }
 
 }
